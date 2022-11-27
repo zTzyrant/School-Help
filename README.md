5920128a74f351a0fa75157a082593e0a19b8368
zTzyrant <61049884+zTzyrant@users.noreply.github.com>
2022-11-28 06:54:05 +0800

    Inputing home page front end

---
+++ README.md
@@ -0,0 +1,21 @@
+# iSchoolHELP
+This project was created to do my college assignment.
+
+This project has 6 use cases:
+ - Register School
+ - Submit Requests
+ - Register as Volunteer
+ - View Requests
+ - Submit Offers
+ - Review Offers
+ 
+ 👷 On This Project 🔨
+ ```
+  PHP version: 8.1.6 
+  Apache/2.4.53 (Win64) OpenSSL/1.1.1n
+  jQuery
+  SweetAlert2
+  Datatables
+  bootstrap 5.2
+ ```
+ >Theme Index: [Mentor](https://bootstrapmade.com/mentor-free-education-bootstrap-theme/) & Theme Dashboard: [Mazer](https://zuramai.github.io/mazer/).
---
+++ index.php
@@ -0,0 +1,250 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ISchool Help</title>
+
+    <!-- Favicons -->
+    <link href="assets/img/logois.png" rel="icon">
+    <link href="assets/img/apple-touch-iconis" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="assets/css/indexstyle.css" rel="stylesheet">
+
+    <!-- =======================================================
+    * Template Name: Mentor - v4.9.1
+    * Template URL: https://bootstrapmade.com/mentor-free-education-bootstrap-theme/
+    * Author: BootstrapMade.com
+    * License: https://bootstrapmade.com/license/
+    ======================================================== -->
+</head>
+<body>
+
+
+    <header id="header" class="fixed-top">
+        <div class="container d-flex align-items-center">
+            <h1 class="logo me-auto"><a href="index.html">ISchoolHELP</a></h1>
+            <!-- Uncomment below if you prefer to use an image logo -->
+            <!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+            <nav id="navbar" class="navbar order-last order-lg-0">
+                <ul>
+                <li><a class="active" href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="contact">Contact</a></li>
+                <li><a href="admin">Admin</a></li>
+                </ul>
+                <i class="bi bi-list mobile-nav-toggle"></i>
+            </nav><!-- .navbar -->
+            <a href="login" class="get-started-btn">Get Started</a>
+        </div>
+    </header>
+
+    <section id="hero" class="d-flex justify-content-center align-items-center">
+        <div class="container position-relative" data-aos="zoom-in" data-aos-delay="100">
+        <h1>Learning Today,<br>Leading Tomorrow</h1>
+        <h2>we are recruiting a tutor to be a Volunteer</h2>
+        <a href="login" class="btn-get-started">Become A Volunteer</a>
+        </div>
+    </section>
+
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about">
+      <div class="container" data-aos="fade-up">
+
+        <div class="row">
+          <div class="col-lg-6 order-1 order-lg-2" data-aos="fade-left" data-aos-delay="100">
+            <img src="assets/img/about.jpg" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content">
+            <h3>Being a tutor with a passion.</h3>
+            <p class="fst-italic">
+             We are recruiting volunteers to become tutors for our website with some benefits.
+            </p>
+            <ul>
+              <li><i class="bi bi-check-circle"></i> Get Experience.</li>
+              <li><i class="bi bi-check-circle"></i> Free Tutor Shirt.</li>
+              <li><i class="bi bi-check-circle"></i> A Certificate of Participation.</li>
+            </ul>
+            <p>
+            Join us and get every available advantage, like the benefits above.
+            </p>
+
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Section -->
+
+    <!-- ======= Counts Section ======= -->
+    <section id="counts" class="counts section-bg">
+      <div class="container">
+
+        <div class="row counters">
+
+          <div class="col-lg-3 col-6 text-center">
+            <span data-purecounter-start="0" data-purecounter-end="1000" data-purecounter-duration="1" class="purecounter"></span>
+            <p>Students</p>
+          </div>
+
+          <div class="col-lg-3 col-6 text-center">
+            <span data-purecounter-start="0" data-purecounter-end="24" data-purecounter-duration="1" class="purecounter"></span>
+            <p>Courses</p>
+          </div>
+
+          <div class="col-lg-3 col-6 text-center">
+            <span data-purecounter-start="0" data-purecounter-end="5" data-purecounter-duration="1" class="purecounter"></span>
+            <p>Events</p>
+          </div>
+
+          <div class="col-lg-3 col-6 text-center">
+            <span data-purecounter-start="0" data-purecounter-end="10" data-purecounter-duration="1" class="purecounter"></span>
+            <p>Trainers</p>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Counts Section -->
+
+    <main id="main">
+        <!-- ======= Trainers Section ======= -->
+        <section id="trainers" class="trainers">
+        <div class="container" data-aos="fade-up">
+            <div class="section-title">
+                <h2>Team</h2>
+                <p>Our Team</p>
+            </div>
+
+            <div class="row" data-aos="zoom-in" data-aos-delay="100">
+            <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+                <div class="member">
+                <img src="assets/img/trainers/trainer-1.png" class="img-fluid" alt="">
+                <div class="member-content">
+                    <h4>Muhammad Zein Akbar</h4>
+                    <span>Website Development</span>
+                    <div class="social">
+                    <a href="https://www.instagram.com/me.zein_/"><i class="bi bi-instagram"></i></a>
+                    <a href="https://www.linkedin.com/in/ztzyrant/"><i class="bi bi-linkedin"></i></a>
+                    </div>
+                </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+                <div class="member">
+                <img src="assets/img/trainers/trainer-2.jpg" class="img-fluid" alt="">
+                <div class="member-content">
+                    <h4>Anak Agung Ari Wijaya</h4>
+                    <span>Project Manager</span>
+                    <div class="social">
+                    <a href="https://www.linkedin.com/in/ari-wijaya-324b9422b/"><i class="bi bi-linkedin"></i></a>
+                    </div>
+                </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+                <div class="member">
+                <img src="assets/img/trainers/trainer-3.jpg" class="img-fluid" alt="">
+                <div class="member-content">
+                    <h4>Nugraha Widi Tara</h4>
+                    <span>Team Project</span>
+                    <div class="social">
+                    <a href="https://www.instagram.com/widi_tara/"><i class="bi bi-instagram"></i></a>
+                    </div>
+                </div>
+                </div>
+            </div>
+
+            </div>
+
+        </div>
+        </section><!-- End Trainers Section -->
+    </main>
+    
+    <!-- ======= Footer ======= -->
+    <footer id="footer">
+      <div class="footer-top">
+        <div class="container">
+            <div class="row">
+
+            <div class="col-lg-3 col-md-6 footer-contact">
+                <h3>ISchool Help</h3>
+                <p>
+                Jl. Raya Puputan No.86 <br>
+                Kota Denpasar, Bali 80234<br>
+                Indonesia <br><br>
+                <strong>Phone:</strong> +62 0821 3492 xxxx<br>
+                <strong>Email:</strong> mzeinakbar@aol.com<br>
+                </p>
+            </div>
+
+            <div class="col-lg-2 col-md-6 footer-links">
+                <h4>Useful Links</h4>
+                <ul>
+                <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
+                <li><i class="bx bx-chevron-right"></i> <a href="admin">Admin</a></li>
+                <li><i class="bx bx-chevron-right"></i> <a href="volunteer">Volunteer</a></li>
+                </ul>
+            </div>
+
+            <div class="col-lg-3 col-md-6 footer-links">
+
+            </div>
+
+            <div class="col-lg-4 col-md-6 footer-newsletter">
+                <h4>Join Our Newsletter</h4>
+                <p>Get the latest news from us. by adding an email into the input below.</p>
+                <form action="" method="post">
+                <input type="email" name="email"><input type="submit" value="Subscribe">
+                </form>
+            </div>
+
+            </div>
+        </div>
+      </div>
+
+      <div class="container d-md-flex py-4">
+
+      <div class="me-md-auto text-center text-md-start">
+          <div class="copyright">
+          &copy; Copyright <strong><span>Mentor</span></strong>. All Rights Reserved
+          </div>
+          <div class="credits">
+          Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+          </div>
+      </div>
+      <div class="social-links text-center text-md-right pt-3 pt-md-0">
+          <a href="https://www.instagram.com/me.zein_/" class="instagram"><i class="bx bxl-instagram"></i></a>
+          <a href="https://www.linkedin.com/in/ztzyrant/" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+      </div>
+      </div>
+    </footer><!-- End Footer -->
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+    <script src="assets/vendor/aos/aos.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="assets/js/main.js"></script>
+    
+</body>
+</html>
